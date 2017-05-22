92ae8d7765156aa28e2466288f32e38d064ed43d
jinxmcg <cristi@whi.ro>
2017-05-23 00:11:13 +0200

    Added test functions

--- bbucket.go
+++ bbucket.go
@@ -1,2 +1,38 @@
 package bbucket
 
+import (
+	"github.com/jinxmcg/quickbites"
+	"unsafe/pointer"
+)
+
+conset (
+	DEFAULT_BUCKET_SIZE = 248
+	DEFAULT_NO_VALUES = 4000000 * DEFAULT_BUCKET_SIZE
+)
+
+type BucketIndex struct {	
+	len int
+	cap int
+}
+
+type bucket struct {
+	bucketId int 
+	content []byte //the bucket
+	maxValue int //max bucket bits
+	nextBucket unsafe.pointer	
+}
+
+func New() *BucketIndex {
+	return &BucketIndex{0,DEFAULT_NO_VALUES}
+}
+
+func (b *BucketIndex) Len() int {
+	return b.len
+}
+
+func (b *BucketIndex) Cap() int {
+	return b.cap
+}
+func (b *BucketIndex) SetBit() uint64 {
+	return true
+}
